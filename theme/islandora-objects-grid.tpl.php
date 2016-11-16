4c2d1a99e66ea72186382bab4a963b5abf35ff63
nruest <ruestn@gmail.com>
2016-11-16 08:24:06 -0500

    YUL customization; http://redmine.library.yorku.ca/issues/1783

--- theme/islandora-objects-grid.tpl.php
+++ theme/islandora-objects-grid.tpl.php
@@ -9,7 +9,10 @@
  <?php foreach($objects as $object): ?>
    <div class="islandora-objects-grid-item">
      <dl class="islandora-object <?php print $object['class']; ?>">
-       <dt class="islandora-object-thumb"><?php print $object['thumb']; ?></dt>
+     <?php if (in_array('islandora:collectionCModel', $object['models'])): ?>
+       <dt class="fa fa-folder-open-o" style="font-size: 6em; color: #777;"></dt> 
+       <?php else: ?><dt class="islandora-object-thumb"><?php print $object['thumb']; ?><?php print $object['model']; ?></dt>
+     <?php endif; ?>
        <dd class="islandora-object-caption"><?php print $object['link']; ?></dd>
      </dl>
    </div>
