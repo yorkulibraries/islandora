6f69ec7daf556f4cff74cd6c6b143bf74592be3d
nruest <ruestn@gmail.com>
2016-11-22 19:44:29 -0500

    Remove cruft.

--- theme/islandora-objects-grid.tpl.php
+++ theme/islandora-objects-grid.tpl.php
@@ -11,7 +11,7 @@
      <dl class="islandora-object <?php print $object['class']; ?>">
      <?php if (in_array('islandora:collectionCModel', $object['models'])): ?>
        <dt class="fa fa-folder-open-o" style="font-size: 6em; color: #777;"></dt> 
-       <?php else: ?><dt class="islandora-object-thumb"><?php print $object['thumb']; ?><?php print $object['model']; ?></dt>
+       <?php else: ?><dt class="islandora-object-thumb"><?php print $object['thumb']; ?></dt>
      <?php endif; ?>
        <dd class="islandora-object-caption"><?php print $object['link']; ?></dd>
      </dl>
