5a5227040aed2626a1e71162a0d35132ab2e18e6
nruest <ruestn@gmail.com>
2017-05-25 11:19:43 -0400

    Include newspaper cmodel for font-awesome.
    Do it better.

--- theme/islandora-objects-grid.tpl.php
+++ theme/islandora-objects-grid.tpl.php
@@ -9,7 +9,8 @@
  <?php foreach($objects as $object): ?>
    <div class="islandora-objects-grid-item">
      <dl class="islandora-object <?php print $object['class']; ?>">
-     <?php if (in_array('islandora:collectionCModel', $object['models'])): ?>
+     <?php $fa_awesome_models = array('islandora:collectionCModel', 'islandora:newspaperCModel'); ?>
+     <?php if (array_intersect($fa_awesome_models, $object['models'])): ?>
        <dt class="fa fa-folder-open-o" style="font-size: 6em; color: #777;"></dt> 
        <?php else: ?><dt class="islandora-object-thumb"><?php print $object['thumb']; ?></dt>
      <?php endif; ?>
